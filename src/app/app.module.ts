7b5b397f7f45b028fb7a5df304294f0a9487b653
Sérgio Piacenti <sergio.piacenti@itixti.com.br>
2021-01-06 08:59:18 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule, FormsModule  } from '@angular/forms';
 import { AlertModule } from './_alert';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConsultasComponent } from './consultas/consultas.component';
